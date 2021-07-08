cd9373bf7939f3d390878ac6bf3d63f5a08e84a1
Jin0331 <sempre813@hallym.ac.kr>
2021-07-08 09:05:01 +0900

    update

--- ena_selenium_parallel.R
+++ ena_selenium_parallel.R
@@ -29,16 +29,10 @@ collection_to_DF_context <- function(collection_name, url) {
   m$find() %>% as_tibble() %>% return()
 }
 run_parse <- function(remDr, ena_url, id, collection_name, start, end){
-  ena_parse <- function(remDr, ena_url, id, sleep_cnt = 10){
+  ena_parse <- function(remDr, ena_url, id, sleep_cnt = 8){
     
     remDr$navigate(paste0(ena_url, id))
     Sys.sleep(sleep_cnt)
-    
-    
-    remDr$findElement(using = "xpath", "//div[contains(text(),'Base')]/../div[2]")
-    
-    
-    
     
     title <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[2]')
     title <- title$getElementText() %>% unlist()
@@ -46,37 +40,78 @@ run_parse <- function(remDr, ena_url, id, collection_name, start, end){
     study_accession <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[4]/div/div[2]/app-read-file-links/div/div[3]/table/tbody/tr/td[1]/div/span/a')
     study_accession <- study_accession$getElementText() %>% unlist()
     
-    organism <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[3]/div[1]/div[2]/a')
-    organism <- organism$getElementText() %>% unlist()
     
-    sample_accession <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[3]/div[2]/div[2]/span')
-    sample_accession <- sample_accession$getElementText() %>% unlist()
+    tryCatch(
+      expr = {
+        organism <- remDr$findElement(using = "xpath", "//div[contains(text(),'Organism')]/../div[2]")
+        organism <- organism$getElementText() %>% unlist()
+      },
+      error = function(e){
+        organism <<- " "
+      })
     
-    instrument_model <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[3]/div[4]/div[2]/span')
-    instrument_model <- instrument_model$getElementText() %>% unlist()
+    tryCatch(
+      expr = {
+        sample_accession <- remDr$findElement(using = "xpath", "//div[contains(text(),'Sample Accession')]/../div[2]")
+        sample_accession <- sample_accession$getElementText() %>% unlist()
+      },
+      error = function(e){
+        sample_accession <<- " "
+      })
     
-    read_count <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[3]/div[5]/div[2]/span')
-    read_count <- read_count$getElementText() %>% unlist()
+    tryCatch(
+      expr = {
+        instrument_model <- remDr$findElement(using = "xpath", "//div[contains(text(),'Instrument Model')]/../div[2]")
+        instrument_model <- instrument_model$getElementText() %>% unlist()
+      },
+      error = function(e){
+        instrument_model <<- " "
+      })
     
-    base_count <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[3]/div[6]/div[2]/span')
-    base_count <- base_count$getElementText() %>% unlist()
+    tryCatch(
+      expr = {
+        read_count <- remDr$findElement(using = "xpath", "//div[contains(text(),'Read')]/../div[2]")
+        read_count <- read_count$getElementText() %>% unlist()
+      },
+      error = function(e){
+        read_count <<- " "
+      })
     
-    if(str_detect(string = base_count, pattern = "[a-zA-Z]")){
-      read_count <- " "
-      base_count <- " "
-      library_layout <- " "
-      library_strategy <- " "
-      library_source <- " "
-    } else {
-      library_layout <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[3]/div[8]/div[2]')
-      library_layout <- library_layout$getElementText() %>% unlist()
-      
-      library_strategy <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[3]/div[9]/div[2]/span')
-      library_strategy <- library_strategy$getElementText() %>% unlist()
-      
-      library_source <- remDr$findElement(using = "xpath", '//*[@id="view-content-col"]/div[3]/div[10]/div[2]')
-      library_source <- library_source$getElementText() %>% unlist()
-    }
+    tryCatch(
+      expr = {
+        base_count <- remDr$findElement(using = "xpath", "//div[contains(text(),'Base')]/../div[2]")
+        base_count <- base_count$getElementText() %>% unlist()
+      },
+      error = function(e){
+        base_count <<- " "
+      })
+    
+    tryCatch(
+      expr = {
+        library_layout <- remDr$findElement(using = "xpath", "//div[contains(text(),'Library Layout')]/../div[2]")
+        library_layout <- library_layout$getElementText() %>% unlist()
+      },
+      error = function(e){
+        library_layout <<- " "
+      })
+    
+    tryCatch(
+      expr = {
+        library_strategy <- remDr$findElement(using = "xpath", "//div[contains(text(),'Library Strategy')]/../div[2]")
+        library_strategy <- library_strategy$getElementText() %>% unlist()
+      },
+      error = function(e){
+        library_strategy <<- " "
+      })
+    
+    tryCatch(
+      expr = {
+        library_source <- remDr$findElement(using = "xpath", "//div[contains(text(),'Library Source')]/../div[2]")
+        library_source <- library_source$getElementText() %>% unlist()
+      },
+      error = function(e){
+        library_source <<- " "
+      })
     
     tibble(title = title,
            study_accession = study_accession, 
@@ -90,6 +125,7 @@ run_parse <- function(remDr, ena_url, id, collection_name, start, end){
            library_source = library_source) %>% 
       return()
   }
+  
   remDr$open() 
   remDr$navigate("https://www.ebi.ac.uk/ena/browser/home")
   Sys.sleep(10)
